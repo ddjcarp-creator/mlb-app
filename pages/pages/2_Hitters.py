b5300fe0013ed1fed5f39e75ab03274959cfa60f
ddjcarp-creator <ddjcarp@gmail.com>
2026-04-29 18:53:06 +1000

    Create 2_Hitters.py

---
+++ pages/pages/2_Hitters.py
@@ -0,0 +1,13 @@
+import streamlit as st
+from utils.statcast_utils import get_statcast_hitter_table
+
+st.title("Hitters")
+
+search = st.text_input("Search hitter")
+
+df = get_statcast_hitter_table()
+
+if search:
+    df = df[df["Hitter"].str.contains(search, case=False)]
+
+st.dataframe(df, use_container_width=True)
